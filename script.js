4d15e707546d119dcfa33089dbe8386261c89ef2
gulmirakhonmusaeva <gulmirakhonmusaeva@gmail.com>
2022-01-24 20:16:20 +0500

    Add all files

---
+++ script.js
@@ -0,0 +1,126 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('.loader')
+
+    setTimeout(() => {
+        loader.style.display = 'none'
+        setTimeout(() => {
+            loader.style.opacity  = '0'
+        }, 500);
+    }, 2000);
+
+    let restart = document.querySelector('#restart'),
+        score = document.querySelector('#score'),
+        choices = document.querySelectorAll('.choice'),
+        modal = document.querySelector('.modal'),
+        result = document.querySelector('#result'),
+        scoreBoard = {
+            player: 0,
+            computer:0 
+        }
+
+
+        // playGame
+        function play(event) { 
+            restart.style.display = 'inline-block'
+            const playerChoice = event.target.id
+            const computerChoice = getComputerChoices()
+            const winner = getWinner(playerChoice, computerChoice)
+            showWinner(winner, computerChoice)
+         }
+
+        // getComputerCHoices
+        function getComputerChoices(){
+            const rand = Math.random()
+            if (rand < 0.34) {
+                return  'rock'
+            } if ( rand <= 0.67){
+                return 'paper'
+            } else {
+                return 'scissors'
+            }
+        }
+
+        //getWinner
+        function getWinner( p, c) {
+            if (p === c){
+                return 'draw'
+            } else if (p === 'rock'){
+                if (c==='paper'){
+                    return 'computer'
+                } else {
+                    return 'player'
+                }
+            } else if (p === 'paper'){
+                if ( c=== 'scissors'){
+                    return 'computer'
+                } else {
+                    return 'player'
+                } 
+            } else if ( p === 'scissors'){
+                if (c === 'rock'){
+                    return 'computer'
+                } else {
+                    return 'player'
+                }
+            }
+        }
+
+        // showWinner
+        function  showWinner( winner, computerChoice) { 
+            if (winner === 'player') {
+                scoreBoard.player++
+                result.innerHTML = `
+                    <h1 class="text-win">You win</h1>
+                    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                    <p> Computer chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong> </p>
+                `
+            } else if (winner === 'computer'){
+                scoreBoard.computer++
+                result.innerHTML = ` 
+                    <h1 class="text-lose">You lose</h1>
+                    <i class =" fas fa-hand-${computerChoice} fa-10x"></i>
+                    <p>Computer chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+                `
+
+            } else {
+                result.innerHTML = `
+                    <h1>It's A Draw</h1>
+                    <i class =" fas fa-hand-${computerChoice} fa-10x"></i>
+                    <p>Computer chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+                `
+            }
+
+            score.innerHTML = ` 
+                <p> Player: ${scoreBoard.player}</p>
+                <p> Computer: ${scoreBoard.computer}</p>
+            `
+            modal.style.display = 'block'
+         }
+
+        //restartGame
+        function restartGame() {
+            scoreBoard.player = 0
+            scoreBoard.computer = 0
+            score.innerHTML = ` 
+                <p> Player: ${scoreBoard.player}</p>
+                <p> Computer: ${scoreBoard.computer}</p>
+            `
+        }
+
+        // clearModal
+        function clearModal(event) {
+           if( event.target == modal){
+               modal.style.display = 'none'
+           }
+        }
+
+        // EVENT 
+        choices.forEach(choice => choice.addEventListener('click', play)) 
+            
+        window.addEventListener('click', clearModal)
+        restart.addEventListener('click',restartGame)
+ 
+
+
+})
